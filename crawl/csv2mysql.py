888f6058dd30dc9cea93d81ea521985808c1e449
indiflex <indiflex1@gmail.com>
2019-01-21 16:54:28 +0900

    csv to mysql with Tx

--- crawl/csv2mysql.py
+++ crawl/csv2mysql.py
@@ -2,7 +2,6 @@ import pymysql
 import csv
 import codecs
 
-
 def get_conn(db):
     return pymysql.connect(
         host='localhost',
@@ -12,8 +11,41 @@ def get_conn(db):
         db=db,
         charset='utf8')
 
-sql_truncate = "truncate table Meltop"
+# sql_truncate = "truncate table Meltop"
+sql_truncate = "delete from Meltop"
 sql_insert = "insert into Meltop(rank, title, singer, likecnt) values(%s,%s,%s,%s)"
+isStart = True
+
+def save(lst):
+    try:
+        conn = get_conn('dadb')
+        conn.autocommit = False
+        cur = conn.cursor()
+
+        global isStart
+        if isStart:
+            cur.execute(sql_truncate)
+            isStart = False
+
+        cur.executemany(sql_insert, lst)
+        conn.commit()
+        print("Affected RowCount is", cur.rowcount, "/", len(lst))
+
+    except Exception as err:
+        conn.rollback()
+        print("Error!!", err)
+
+    finally:
+        try:
+            cur.close()
+        except:
+            print("Error on close cursor")
+
+        try:
+            conn.close()
+        except Exception as err2:
+            print("Fail to connect!!", err2)
+
 
 csvFile = codecs.open("../crawl/data/meltop100.csv", "r", "utf-8")
 reader = csv.reader(csvFile, delimiter=',', quotechar='"')
@@ -27,9 +59,4 @@ print("11>>", lst[1])
 del lst[0]
 del lst[ len(lst) - 1 ]
 
-conn = get_conn('dadb')
-with conn:
-    cur = conn.cursor()
-    cur.execute(sql_truncate)
-    cur.executemany(sql_insert, lst)
-    print("Affected RowCount is", cur.rowcount)
+save(lst)
