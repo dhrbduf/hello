10a9526cfdc6842d1e54a9b1337ff6c80fb4d24a
indiflex <indiflex1@gmail.com>
2019-01-30 16:15:16 +0900

    naver book to mongodb

--- crawl/naver_book.py
+++ crawl/naver_book.py
@@ -1,26 +1,52 @@
 import requests, json
 from bs4 import BeautifulSoup
+from pymongo import MongoClient, DESCENDING
+from pprint import pprint
 
-url = "https://openapi.naver.com/v1/search/book.json"
+def main():
+    # jsonData = crawl("파이썬")
 
-title = "파이썬"
-params = {
-    "query": title,
-    "display": 20,
-    "start": 1,
-    "sort": "date"
-}
+    mongo_client = MongoClient('localhost', 27017)
+    db = mongo_client.dooodb
+    # save2Mongo(db, jsonData['items'])
+    printBooks(db)
 
-headers = {
-    "X-Naver-Client-Id": "h8D7yhnqUBsVaCHQOViJ",
-    "X-Naver-Client-Secret": "TYwzilDXGl"
-}
+def printBooks(db):
+    results = db.Books.find({}).sort("price", DESCENDING).limit(10)
+    for r in results:
+        pprint(r)
 
-result = requests.get(url, params=params, headers=headers).text
+def save2Mongo(db, docs):
+    collection = db.Books
+    datacnt = len(docs)
+    print("data count =", datacnt)
+    collection.delete_many({})
+    result = collection.insert_many(docs)
+    print("ids =",result.inserted_ids)
+    insertedCount = len(result.inserted_ids)
+    print('Affected docs is {}'.format(insertedCount))
 
-jsonData = json.loads(result)
+    if datacnt != insertedCount:
+        print("Error!!!", datacnt, insertedCount)
 
-print(json.dumps(jsonData, ensure_ascii=False, indent=2))
+def crawl(title, page=1):
+    url = "https://openapi.naver.com/v1/search/book.json"
+    params = {
+        "query": title,
+        "display": 100,
+        "start": page,
+        "sort": "date"
+    }
 
-# for item in jsonData['items']:
-#     item['']
+    headers = {
+        "X-Naver-Client-Id": "h8D7yhnqUBsVaCHQOViJ",
+        "X-Naver-Client-Secret": "TYwzilDXGl"
+    }
+
+    result = requests.get(url, params=params, headers=headers).text
+    jsonData = json.loads(result)
+    print(json.dumps(jsonData, ensure_ascii=False, indent=2))
+    return jsonData
+
+if __name__ == '__main__':
+    main()
